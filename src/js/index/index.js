eae0d736c75a639ccd9e8885d6846462dabc038b
Wang Gao Jie <279816334@qq.com>
2021-03-19 11:57:47 +0800

    v0.0.0

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -4,6 +4,11 @@ import Swiper from '../library/swiper.js';
 $(function () {
     // banner 轮播图
     let mySwiper = new Swiper('.swiper-container', {
+        autoplay: {
+            delay: 2000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+        },
         direction: 'horizontal', // 垂直切换选项
         loop: true, // 循环模式选项
         effect: "fade",
@@ -34,21 +39,49 @@ $(function () {
 
     // right1 轮播图
     var mySwiper1 = new Swiper('.swiper-container1', {
-        autoplay: true,//可选选项，自动滑动
+        autoplay: {
+            delay: 5000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+        },
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '._prev1',
+            prevEl: '._prev1',
         },
         effect: "fade",
         loop: true,
 
     })
-
-
-
-
-
     // ============================================================================
+    // jd_seckill center 轮播图
+    // center
+    var mySwiper2 = new Swiper('.swiper-container2', {
+        // autoplay: true,//可选选项，自动滑动
+        navigation: {
+            nextEl: '._prev2',
+            prevEl: '._prev2',
+        },
+        loop: true,
+        speed: 800,
+        allowTouchMove: false,
+    })
+
+    // right
+    var mySwiper3 = new Swiper('.swiper-container3', {
+        autoplay: {
+            delay: 1000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+        },
+        loop: true,
+        speed: 600,
+        allowTouchMove: false,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    })
+    // ============================================================================
+
 
 })
 
